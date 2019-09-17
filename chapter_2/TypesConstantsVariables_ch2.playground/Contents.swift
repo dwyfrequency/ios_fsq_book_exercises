c966845ccee55e3d4c6925f56075268bc1c412f5
Jack Dwyer <jdwy215@gmail.com>
2019-09-17 10:48:36 -0400

    adding ch 1 - 3

---
+++ chapter_2/TypesConstantsVariables_ch2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let numOfStopLights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+var unemployment: Float = 0.23
+let townDescr = "\(townName) has a population of \(population) and \(numOfStopLights) stoplights. It has \(unemployment)% unemployment"
+print(townDescr)
+
+print("blem")
---
+++ chapter_3/Conditionals.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var population: Int = 50444
+var message: String
+var hasPostOffice: Bool = true
+if population < 10000 {
+    message = "\(population) is a small town!"
+} else if population >= 10000 && population < 50000 {
+    message = "\(population) is a medium town!"
+} else if population >= 50000 && population < 100000 {
+    message = "\(population) is a medium city!"
+} else {
+    message = "\(population) is pretty big!"
+}
+print(message)
+if !hasPostOffice {
+    print("Where do we buy stamps?")
+}
