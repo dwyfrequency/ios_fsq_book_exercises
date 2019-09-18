6eabe28e20e020b4ec82aa93f45cb5d60e02129b
Jack Dwyer <jdwy215@gmail.com>
2019-09-17 22:37:43 -0400

    adding switch

--- chapter_5/Switch.playground/Contents.swift
+++ chapter_5/Switch.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var str = "Hello, playground"
+func getErrorCode(errorCode errCode: Int, _ errString: String = "The request failed with the error:") -> (Int, String){
+    var errorString = errString
+    switch errCode {
+    case 100, 101:
+        errorString += " Informational, \(statusCode)."
+    case 204:
+        errorString += " Successful but no content, 204."
+    case 300...307:
+        errorString += " Redirection, \(statusCode)."
+    case 400...417:
+        errorString += " Client error, \(statusCode)."
+    case 500...505:
+        errorString += " Server error, \(statusCode)."
+    case let unknownCode where (unknownCode >= 200 && unknownCode < 300)
+        || unknownCode > 505:
+        errorString = "\(unknownCode) is not a known error code."
+    default:
+        errorString = "Unexpected error encountered."
+    }
+    return (errCode, errorString)
+}
+
+var statusCode: Int = 204
+//var errorString: String = "The request failed with the error:"
+getErrorCode(errorCode: statusCode)
+let tup = getErrorCode(errorCode: 291)
+print(tup)
